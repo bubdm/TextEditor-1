ac3e21d5abaece2ee0e11197c2b83c43f2d64dc7
Starlk <starlkytminecraft@gmail.com>
2021-06-12 00:59:25 +0300

    Bug fix.

--- TextEditorWPF/Pages/ShellViewModel.cs
+++ TextEditorWPF/Pages/ShellViewModel.cs
@@ -21,7 +21,8 @@ namespace TextEditorWPF.Pages
             {
                 SetAndNotify(ref selectedFile, value);
                 CanSave = SelectedFile != null;
-                value.IsSaved = false;
+
+                if (value != null) value.IsSaved = false;
             }
         }
 
